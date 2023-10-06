92fb01f26a53dead7545027773f422cb645af627
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-06 17:15:58 +0600

    Setup User Creation Requirements

--- server/Controllers/authController.js
+++ server/Controllers/authController.js
@@ -1,6 +1,56 @@
+import User from "../models/UserSchema.js";
+import Doctor from "../models/DoctorSchema.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+
 export const register = async (req, res) => {
+	const { name, email, password, role, photo, gender } = req.body;
+
 	try {
-	} catch (err) {}
+		let user = null;
+
+		if (role === "patient") {
+			user = User.findOne({ email });
+		} else if (role === "doctor") {
+			user = Doctor.findOne({ email });
+		}
+
+		// ======== Existing User Check ========
+		if (user) {
+			return res.status(400).json({ message: "User already exists" });
+		}
+
+		// ======== Hash Password ========
+		const salt = await bcrypt.genSalt(10);
+		const hashPassword = await bcrypt.hash(password, salt);
+
+		if (role === "patient") {
+			user = new User({
+				name,
+				email,
+				password: hashPassword,
+				photo,
+				gender,
+				role,
+			});
+		}
+
+		if (role === "doctor") {
+			user = new Doctor({
+				name,
+				email,
+				password: hashPassword,
+				photo,
+				gender,
+				role,
+			});
+		}
+
+		await user.save();
+		res.status(200).json({ success: true, message: "User created successfully" });
+	} catch (err) {
+		res.status(500).json({ success: false, message: "Internal server error, Try again" });
+	}
 };
 
 export const login = async (req, res) => {
