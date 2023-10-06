b1280bccfc7e0c32601b8e9a27e2a109ff5210fe
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-06 15:54:32 +0600

    Connected With MongoDB Database

--- server/index.js
+++ server/index.js
@@ -1,12 +1,11 @@
-import express from "express";
-import cookieParser from "cookie-parser";
 import cors from "cors";
-import mongoose from "mongoose";
 import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
-// ========== Middleware ==========
 const app = express();
 const port = process.env.PORT || 8000;
 
@@ -14,10 +13,31 @@ const corsOptions = {
 	origin: true,
 };
 
+// ========== Database Connection ==========
+mongoose.set("strictQuery", false);
+
+const connectDB = async () => {
+	try {
+		await mongoose.connect(process.env.MONGODB_URL, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
+		console.log("Connected to MongoDB Database");
+	} catch (err) {
+		console.log("Failed to Connected With MongoDB Database");
+	}
+};
+
+// ========== Middleware ==========
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors(corsOptions));
+
 app.get("/", (req, res) => {
 	res.send("Medicare Booking Server is running...");
 });
 
 app.listen(port, () => {
+	connectDB();
 	console.log(`Medicare Booking Server is running on port ${port}`);
 });
