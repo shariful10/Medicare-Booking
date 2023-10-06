b0fb27a6a6bca950dad9c355ddec0f6779435029
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-06 21:30:36 +0600

    Login Implement On Server Side

--- server/Controllers/authController.js
+++ server/Controllers/authController.js
@@ -3,6 +3,12 @@ import Doctor from "../models/DoctorSchema.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 
+const generateToken = (user) => {
+	return jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET_KEY, {
+		expiresIn: "15d",
+	});
+};
+
 export const register = async (req, res) => {
 	const { name, email, password, role, photo, gender } = req.body;
 
@@ -54,6 +60,47 @@ export const register = async (req, res) => {
 };
 
 export const login = async (req, res) => {
+	const { email } = req.body;
+
 	try {
-	} catch (err) {}
+		let user = null;
+		const patient = await User.findOne({ email });
+		const doctor = await Doctor.findOne({ email });
+
+		if (patient) {
+			user = patient;
+		}
+
+		if (doctor) {
+			user = doctor;
+		}
+
+		// ======== Check The User Exist or Not ========
+		if (!user) {
+			return res.status(404).json({ message: "User not found" });
+		}
+
+		// ======== Compare Password ========
+		const isPasswordMatch = await bcrypt.compare(
+			req.body.password, 
+			user.password
+		);
+
+		if (!isPasswordMatch) {
+			return res.status(400).json({ status: false, message: "Invelid crediential" });
+		}
+
+		// ======== Get Token ========
+		const token = generateToken(user);
+		const { password, role, appoinments, ...rest } = user._doc;
+		res.status(200).json({
+			status: true,
+			message: "Successfully login",
+			token,
+			data: { ...rest },
+			role,
+		});
+	} catch (err) {
+		return res.status(500).json({ status: false, message: "Failed to login" });
+	}
 };
